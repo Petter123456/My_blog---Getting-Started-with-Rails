fd6aac0c77f56eea76ed9953093b04f3c164f08a
Petter Fagerlund <petter.fagerlund@gmail.com>
2018-01-03 15:34:54 -0500

    My blog final commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :comments
+  
+  get 'welcome/index'
+
+  resources :articles
+
+  root 'welcome#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+end
